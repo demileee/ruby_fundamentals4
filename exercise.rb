87a59b06ee6356a740b0a30642a9c3428a6bd9c2
Demi Lee <demilee.to@gmail.com>
2017-09-20 15:56:00 -0400

    exercise 0 - define collections

---
+++ exercise.rb
@@ -0,0 +1,33 @@
+
+colors = ["white", "black", "red", "blue"]
+ages = [20, 23, 24, 26]
+coinflips = ["heads", "tails", "heads", "tails"]
+artists = ["mariah", "whitney", "toni", "michael"]
+colors2 = [:white, :black, :red, :blue]
+
+dictionary = {
+  verb: "An action.",
+  noun: "A thing.",
+  adjective: "A descriptor."
+}
+
+movies = {
+  up: 2010,
+  inception: 2011,
+  notebook: 1990
+}
+
+cities = {
+  toronto: "2 million",
+  hongkong: "1 billion",
+  montreal: "1 million"
+}
+
+people = {
+  bob: 20,
+  stacy: 24,
+  patty: 19
+}
+
+puts colors
+puts cities
