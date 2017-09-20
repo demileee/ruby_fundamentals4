a979d78bea015ced3e00dc9977a4481e266e486e
Demi Lee <demilee.to@gmail.com>
2017-09-20 16:08:12 -0400

    exercise 2

--- exercise.rb
+++ exercise.rb
@@ -29,8 +29,18 @@ people = {
   patty: 19
 }
 
+#exercise1
 puts coinflips   #1
 puts colors[0]   #2
 puts ages.sort   #3
 ages << 0        #4
 puts movies[:up] #5
+
+#exercise2
+puts colors[-1]                        #1
+cities[:tokyo] = "5 million"           #2
+revcoin = coinflips.reverse            #3
+puts cities[:tokyo]                    #4
+artists.each do |artist|               #5
+  puts "I think #{artist} is great."
+end
